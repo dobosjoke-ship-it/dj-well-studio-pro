866c5921e1f4cff6af4b3fbf507047c46adbac90
József Dobos <dobosjoke@gmail.com>
2026-07-05 22:41:54 +0200

    Fix GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    host: '0.0.0.0'
-  }
+  base: '/dj-well-studio-pro/',
 });
